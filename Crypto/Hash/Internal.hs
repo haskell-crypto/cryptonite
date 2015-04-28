87e2862eaaa11b920a0bb067969587ffd14430b9
Vincent Hanquez <vincent@snarc.org>
2015-04-28 11:39:23 +0100

    remove stale file

--- Crypto/Hash/Internal.hs
+++
@@ -1,9 +0,0 @@
--- |
--- Module      : Crypto.Hash.Internal
--- License     : BSD-style
--- Maintainer  : Vincent Hanquez <vincent@snarc.org>
--- Stability   : experimental
--- Portability : unknown
-module Crypto.Hash.Internal
-    (
-    ) where
