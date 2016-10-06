ad52a38abcf0b8910f0705926a5fa4fd400625de
Kazu Yamamoto <kazu@iij.ad.jp>
2016-10-06 14:41:09 +0900

    switching ECDH implementation based on Crypto.ECC.

--- Crypto/ECC.hs
+++ Crypto/ECC.hs
@@ -19,9 +19,10 @@ module Crypto.ECC
     , SharedSecret(..)
     ) where
 
+import           Crypto.Number.Serialize (i2ospOf_)
 import qualified Crypto.PubKey.ECC.P256 as P256
-import qualified Crypto.PubKey.ECC.Types as H
 import qualified Crypto.PubKey.ECC.Prim as H
+import qualified Crypto.PubKey.ECC.Types as H
 import           Crypto.Random
 import           Crypto.Internal.Imports
 import           Crypto.Internal.ByteArray (ByteArrayAccess, ScrubbedBytes)
@@ -97,11 +98,15 @@ instance EllipticCurve Curve_P256R1 where
     curveGenerateScalar = P256Scalar <$> P256.scalarGenerate
     curveGenerateKeyPair = toKeyPair <$> P256.scalarGenerate
       where toKeyPair scalar = KeyPair (P256Point $ P256.toPoint scalar) (P256Scalar scalar)
+
 instance EllipticCurveArith Curve_P256R1 where
     pointAdd  a b = P256Point $ (P256.pointAdd `on` unP256Point) a b
     pointSmul s p = P256Point $ P256.pointMul (unP256Scalar s) (unP256Point p)
+
 instance EllipticCurveDH Curve_P256R1 where
-    ecdh s p = undefined
+    ecdh s p = SharedSecret $ i2ospOf_ 32 x -- FIXME: 32 is correct?
+      where
+        (x, _) = P256.pointToIntegers $ P256.pointMul (unP256Scalar s) (unP256Point p)
 
 data Curve_P521R1 = Curve_P521R1
 
@@ -115,7 +120,13 @@ instance EllipticCurve Curve_P521R1 where
     curveGenerateScalar = P521Scalar <$> H.scalarGenerate (H.getCurveByName H.SEC_p521r1)
     curveGenerateKeyPair = toKeyPair <$> H.scalarGenerate (H.getCurveByName H.SEC_p521r1)
       where toKeyPair scalar = KeyPair (P521Point $ H.pointBaseMul (H.getCurveByName H.SEC_p521r1) scalar) (P521Scalar scalar)
+
 instance EllipticCurveArith Curve_P521R1 where
     pointAdd a b = P521Point $ (H.pointAdd (H.getCurveByName H.SEC_p521r1) `on` unP521Point) a b
     pointSmul s p = P521Point (H.pointMul (H.getCurveByName H.SEC_p521r1) (unP521Scalar s) (unP521Point p))
 
+instance EllipticCurveDH Curve_P521R1 where
+    ecdh s p = SharedSecret $ i2ospOf_ ((nbBits + 7) `div` 8) x
+      where
+        H.Point x _ = H.pointMul (H.getCurveByName H.SEC_p521r1) (unP521Scalar s) (unP521Point p)
+        nbBits    = H.curveSizeBits $ H.getCurveByName H.SEC_p521r1 -- FIXME: constant?
--- Crypto/PubKey/ECC/DH.hs
+++ Crypto/PubKey/ECC/DH.hs
@@ -9,40 +9,26 @@
 --
 module Crypto.PubKey.ECC.DH
     (
-      Curve
-    , PublicPoint
-    , PrivateNumber
-    , SharedKey(..)
-    , generatePrivate
-    , calculatePublic
+      Scalar
+    , Point
+    , KeyPair(..)
+    , SharedSecret(..)
+    , generateKeyPair
     , getShared
     ) where
 
-import Crypto.Number.Generate (generateMax)
-import Crypto.Number.Serialize (i2ospOf_)
-import Crypto.PubKey.ECC.Prim (pointMul)
+import Crypto.ECC
 import Crypto.Random.Types
-import Crypto.PubKey.DH (SharedKey(..))
-import Crypto.PubKey.ECC.Types (PublicPoint, PrivateNumber, Curve, Point(..), curveSizeBits)
-import Crypto.PubKey.ECC.Types (ecc_n, ecc_g, common_curve)
 
--- | Generating a private number d.
-generatePrivate :: MonadRandom m => Curve -> m PrivateNumber
-generatePrivate curve = generateMax n
-  where
-    n = ecc_n $ common_curve curve
+-- | Generating a pair of private key (scalar d) and public key (point Q).
+generateKeyPair :: (MonadRandom m, EllipticCurveDH curve)
+                => m (KeyPair curve)
+generateKeyPair = curveGenerateKeyPair
 
--- | Generating a public point Q.
-calculatePublic :: Curve -> PrivateNumber -> PublicPoint
-calculatePublic curve d = q
-  where
-    g = ecc_g $ common_curve curve
-    q = pointMul curve d g
-
--- | Generating a shared key using our private number and
---   the other party public point.
-getShared :: Curve -> PrivateNumber -> PublicPoint -> SharedKey
-getShared curve db qa = SharedKey $ i2ospOf_ ((nbBits + 7) `div` 8) x
-  where
-    Point x _ = pointMul curve db qa
-    nbBits    = curveSizeBits curve
+--- | Generating a shared key using our private key and
+---   the public key of the other party.
+getShared :: EllipticCurveDH curve
+          => Scalar curve -- ^ The private key of the receiver
+          -> Point curve  -- ^ The public key of the sender
+          -> SharedSecret
+getShared = ecdh
