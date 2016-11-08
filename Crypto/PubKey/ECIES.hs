13c670e43938f20ee6b9c9c42477364fa62d738d
Kazu Yamamoto <kazu@iij.ad.jp>
2016-11-08 13:38:59 +0900

    implementing ECDH in the ECIES style.

--- Crypto/PubKey/ECIES.hs
+++ Crypto/PubKey/ECIES.hs
@@ -7,27 +7,37 @@
 --
 -- IES with Elliptic curve <https://en.wikipedia.org/wiki/Integrated_Encryption_Scheme>
 --
-module Crypto.PubKey.ECIES
-    ( deriveEncrypt
+module Crypto.PubKey.ECIES (
+      Curve(..)
+    , EllipticCurve(..)
+    , Curve_P256R1(..)
+    , Curve_P384R1(..)
+    , Curve_P521R1(..)
+    , KeyPair(..)
+    , CurveKeyPair(..)
+    , SharedSecret(..)
+    , deriveEncrypt
     , deriveDecrypt
+    , curveBytes
     ) where
 
-import           Crypto.ECC
-import           Crypto.Random
+import Crypto.ECC
+import Crypto.Number.Serialize (i2ospOf_)
+import Crypto.Random.Types
 
--- | Generate random a new Shared secret and the associated point
--- to do a ECIES style encryption
-deriveEncrypt :: (MonadRandom randomly, EllipticCurveDH curve)
-              => Point curve -- ^ the public key of the receiver
-              -> randomly (Point curve, SharedSecret)
-deriveEncrypt pub = do
-    (KeyPair rPoint rScalar) <- curveGenerateKeyPair
-    return (rPoint, ecdh rScalar pub)
+deriveEncrypt :: MonadRandom randomly => Curve -> randomly CurveKeyPair
+deriveEncrypt = generateKeyPair
 
--- | Derive the shared secret with the receiver key
--- and the R point of the scheme.
-deriveDecrypt :: EllipticCurveDH curve
-              => Point curve  -- ^ The received R (supposedly, randomly generate on the encrypt side)
-              -> Scalar curve -- ^ The secret key of the receiver
+--- | Generating a shared key using our private key and
+---   the public key of the other party.
+deriveDecrypt :: (Integer, Integer) -- ^ The public key of the sender
+              -> CurveKeyPair       -- ^ The private key of the receiver
               -> SharedSecret
-deriveDecrypt point secret = ecdh secret point
+deriveDecrypt (x,y) (CurveKeyPair kp) = shared
+  where
+    s = keypairPrivate kp
+    curve = curveOfScalar s
+    p = curveIntegersToPoint curve x y
+    (x', _) = curvePointToIntegers $ curvePointSmul s p
+    len = curveBytes $ curveOfScalar s
+    shared = SharedSecret $ i2ospOf_ len x'
