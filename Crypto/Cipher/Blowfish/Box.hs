ff8a1c524dbcc5da5e32adfe9e9a01f371898788
Lars Petersen <info@lars-petersen.net>
2018-05-08 22:08:20 +0200

    Extend the internal interface of the Blowfish module.
    In preparation of an implementation of the bcrypt_pbkdf (a
    variant of PBKDF2 used by OpenSSH) algorithm,
    certain low-level operations of the Blowfish algorithm need to
    be generalized and exposed.
    
    The Blowfish.Primitive module has already been extended to
    account for the requirements imposed by the BCrypt algorithm,
    but the salt length was limited to 16 bytes and the BCrypt
    specific key schedule setup has been hard-coded into the Blowfish
    module.
    
    This commit makes a clear distintion between the expandKey and
    expandKeyWithSalt operation. Both take arbitrary sized salts
    and keys now. The specialized operation for 16 byte salts as used
    by BCrypt has been preserved and is selected automatically.
    Also, the BCrypt specific parts have been move to the BCrypt
    module with regard to separation of concern.
    
    A benchmark for generating BCrypt hashes with cost 10 shows a
    performance improvement from 158 to 141ms on average (Intel i5-6500)
    after this refactoring.
    Further experiments suggest that the specialized expandKeyWithSalt128
    does not have any advantage over the generalized version
    and might be removed in favour of less branches and exceptional
    behaviour.

--- Crypto/Cipher/Blowfish/Box.hs
+++ Crypto/Cipher/Blowfish/Box.hs
@@ -5,15 +5,19 @@
 -- Portability : Good
 {-# LANGUAGE MagicHash #-}
 module Crypto.Cipher.Blowfish.Box
-    ( createKeySchedule
+    (   KeySchedule(..)
+    ,   createKeySchedule
     ) where
 
-import Crypto.Internal.WordArray (mutableArray32FromAddrBE, MutableArray32)
+import           Crypto.Internal.WordArray (MutableArray32,
+                                            mutableArray32FromAddrBE)
+
+newtype KeySchedule = KeySchedule MutableArray32
 
 -- | Create a key schedule mutable array of the pbox followed by
 -- all the sboxes.
-createKeySchedule :: IO MutableArray32
-createKeySchedule = mutableArray32FromAddrBE 1042 "\
+createKeySchedule :: IO KeySchedule
+createKeySchedule = KeySchedule `fmap` mutableArray32FromAddrBE 1042 "\
     \\x24\x3f\x6a\x88\x85\xa3\x08\xd3\x13\x19\x8a\x2e\x03\x70\x73\x44\
     \\xa4\x09\x38\x22\x29\x9f\x31\xd0\x08\x2e\xfa\x98\xec\x4e\x6c\x89\
     \\x45\x28\x21\xe6\x38\xd0\x13\x77\xbe\x54\x66\xcf\x34\xe9\x0c\x6c\
