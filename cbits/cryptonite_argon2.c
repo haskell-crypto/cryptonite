b21b47e0d72d4b731e05870571d61ee25e623f97
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2018-05-26 06:27:05 -0400

    Support building against system argon2.

--- cbits/cryptonite_argon2.c
+++ cbits/cryptonite_argon2.c
@@ -8,8 +8,14 @@ int cryptonite_argon2_hash_haskell(const uint32_t t_cost,
                                    const size_t hashlen, argon2_type type,
                                    const uint32_t version)
 {
+#ifdef USE_SYSTEM_LIBRARY
+	return argon2_hash(t_cost, m_cost, parallelism, pwd, pwdlen,
+	                   salt, saltlen, hash, hashlen, NULL, 0, type,
+		           version);
+#else
 	return cryptonite_argon2_hash(t_cost, m_cost, parallelism, pwd, pwdlen,
 	                              salt, saltlen, hash, hashlen, type,
 	                              version);
+#endif
 }
 
